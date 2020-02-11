e3cf68eda66185da71898b4c26d697e807f4af53
Ilari Tuominen <ilari.tuominen@gmail.com>
2020-02-11 15:04:09 +0200

    modified connect.php

--- connect.php
+++ connect.php
@@ -2,9 +2,9 @@
 function connect_db() {
  $servername = "localhost";
  // EDIT HERE YOUR USERNAME AND PASSWORD
- $username = "cards";
- $password = "password";
- $dbname = "cards";
+ $username = "bgg282";
+ $password = "moQY1y73d";
+ $dbname = "bgg282";
  // Create connection
  $conn = new mysqli($servername, $username, $password, $dbname);
  // Check connection
